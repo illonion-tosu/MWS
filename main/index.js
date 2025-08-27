9075b0ef244ae653f70b61671a88d5d043034bf3
Lawrence Zhou <60532041+illonion@users.noreply.github.com>
2025-08-27 19:24:15 +1000

    Add warmups

--- main/index.js
+++ main/index.js
@@ -1,11 +1,22 @@
 // Set Total Max HP
 const hpNumberLeftEl = document.getElementById("hp_number_left")
 const hpNumberRightEl = document.getElementById("hp_number_right")
+const hpNegativeNumberLeftEl = document.getElementById("hp_negative_number_left")
+const hpNegativeNumberRightEl = document.getElementById("hp_negative_number_right")
+
+// Animation
+const animation = {
+    "hpNumberLeft": new CountUp(hpNumberLeftEl, 0, 0, 0, 0.2, { useEasing: true, useGrouping: true, separator: ",", decimal: "." }),
+    "hpNumberRight": new CountUp(hpNumberRightEl, 0, 0, 0, 0.2, { useEasing: true, useGrouping: true, separator: ",", decimal: "." }),
+    "hpNegativeNumberLeft": new CountUp(hpNegativeNumberLeftEl, 0, 0, 0, 0.2, { useEasing: true, useGrouping: true, separator: ",", decimal: ".", suffix: ")", prefix: "(" }),
+    "hpNegativeNumberRight": new CountUp(hpNegativeNumberRightEl, 0, 0, 0, 0.2, { useEasing: true, useGrouping: true, separator: ",", decimal: ".", suffix: ")", prefix: "(" })
+}
+
 let totalMaxHp = 0
 let leftHpBeforeMap, rightHpBeforeMap
 let allBeatmaps
 async function getBeatmaps() {
-    const response = await axios.get("_data/beatmaps.json")
+    const response = await axios.get("../_data/beatmaps.json")
     allBeatmaps = response.data.beatmaps
     switch (response.data.roundName) {
         case "RO64": case "RO32": case "RO16":
@@ -22,8 +33,20 @@ async function getBeatmaps() {
     leftHpBeforeMap = totalMaxHp
     rightHpBeforeMap = totalMaxHp
 
-    hpNumberLeftEl.textContent = leftHpBeforeMap.toLocaleString()
-    hpNumberRightEl.textContent = rightHpBeforeMap.toLocaleString()
+    animation.hpNumberLeft.update(leftHpBeforeMap)
+    animation.hpNumberRight.update(rightHpBeforeMap)
+    hpNegativeNumberLeftEl.style.display = "none"
+    hpNegativeNumberRightEl.style.display = "none"
 }
 
 getBeatmaps()
+
+// Warmup
+const toggleWarmupEl = document.getElementById("toggle_warmup")
+let isWarmupToggled = false
+function toggleWarmup() {
+    isWarmupToggled = !isWarmupToggled
+    toggleWarmupEl.textContent = `Toggle Warmup: ${isWarmupToggled? "ON" : "OFF"}`
+    toggleWarmupEl.classList.add(isWarmupToggled? "on" : "off")
+    toggleWarmupEl.classList.remove(isWarmupToggled? "off" : "on")
+}
