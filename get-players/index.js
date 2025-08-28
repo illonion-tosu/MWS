4e44c410efffe1e8af45a9dc3b23e64b11082a22
Lawrence Zhou <60532041+illonion@users.noreply.github.com>
2025-08-28 10:30:38 +1000

    Get Team

---
+++ get-players/index.js
@@ -0,0 +1,21 @@
+const textarea = document.getElementById("textarea")
+const teams = []
+function submit() {
+    const textareaValue = textarea.value
+    const textareaValueSplit = textareaValue.split("\n")
+    for (let i = 0; i < textareaValueSplit.length; i++) {
+        const textareaValueSplitIndividual = textareaValueSplit[i].split(",")
+        const teamData = {
+            playerId: Number(textareaValueSplitIndividual[0]),
+            playerResonance: textareaValueSplitIndividual[1].split("")[8]
+        }
+        teams.push(teamData)
+    }
+
+    const jsonString = JSON.stringify(teams, null, 4);
+    const blob = new Blob([jsonString], { type: "application/json" });
+    const link = document.createElement("a");
+    link.href = URL.createObjectURL(blob);
+    link.download = "players.json";
+    link.click();
+}
